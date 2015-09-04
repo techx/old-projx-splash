1f8cdea24c109da78cd759d5c6263e456b6249f3
JerryLeiDing <leiding@mit.edu>
2015-09-04 16:28:33 -0400

    Added footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
   // Navbar
   var $bodytag = $('html, body');
   var $gotoTags = $('#goto-explore, #goto-faq');
@@ -7,3 +8,15 @@
       scrollTop: $('#'+elementName).offset().top
     }, 400);
   });
+  
+  //Elevator
+    var elevator = new Elevator({
+    mainAudio: '/sound/elevator.mp3',
+    endAudio: '/sound/ding.mp3',
+    duration: 4000
+  });
+  
+  $('#elevator').click(function(e){
+    elevator.elevate();
+  });
+});
